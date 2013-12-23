7e5ae200eb6a6abf3c9c64d748fa4cb3c2adbe7a
cankeyyin <yellow@yellowmatoMacBook-Air.local>
2013-12-23 15:46:36 +0800

    header footer完毕

--- header.js
+++ header.js
@@ -1,9 +1,8 @@
-<!--分享-->
-document.writeln("<div class=\"bshare-custom\"><a title=\"分享到\" href=\"http://www.bShare.cn/\" id=\"bshare-shareto\" class=\"bshare-more\">分享到</a><a title=\"分享到QQ空间\" class=\"bshare-qzone\">QQ空间</a><a title=\"分享到新浪微博\" class=\"bshare-sinaminiblog\">新浪微博</a><a title=\"分享到人人网\" class=\"bshare-renren\">人人网</a><a title=\"分享到腾讯微博\" class=\"bshare-qqmb\">腾讯微博</a><a title=\"分享到网易微博\" class=\"bshare-neteasemb\">网易微博</a><a title=\"更多平台\" class=\"bshare-more bshare-more-icon more-style-addthis\"></a><span class=\"BSHARE_COUNT bshare-share-count\">0</span></div><script type=\"text/javascript\" charset=\"utf-8\" src=\"http://static.bshare.cn/b/buttonLite.js#style=-1&amp;uuid=&amp;pophcol=2&amp;lang=zh\"></script><script type=\"text/javascript\" charset=\"utf-8\" src=\"http://static.bshare.cn/b/bshareC0.js\"></script>");
-document.writeln("<br />");
-document.writeln("<hr />");
-document.writeln("<p>© 2012 天香嘉韵.</p>");
 
-<!--百度统计-->
-var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F91c4162d2211f5bbead7f3b8f12fbd8d' type='text/javascript'%3E%3C/script%3E"));
+document.writeln("<p><a href=\"index.html\">返回首页</a></p>");
+document.writeln("");
+
+<!--分享页面-->
+document.writeln("<div class=\"bshare-custom\"><a title=\"分享到\" href=\"http://www.bShare.cn/\" id=\"bshare-shareto\" class=\"bshare-more\">分享到</a><a title=\"分享到QQ空间\" class=\"bshare-qzone\">QQ空间</a><a title=\"分享到新浪微博\" class=\"bshare-sinaminiblog\">新浪微博</a><a title=\"分享到人人网\" class=\"bshare-renren\">人人网</a><a title=\"分享到腾讯微博\" class=\"bshare-qqmb\">腾讯微博</a><a title=\"分享到网易微博\" class=\"bshare-neteasemb\">网易微博</a><a title=\"更多平台\" class=\"bshare-more bshare-more-icon more-style-addthis\"></a><span class=\"BSHARE_COUNT bshare-share-count\">0</span></div><script type=\"text/javascript\" charset=\"utf-8\" src=\"http://static.bshare.cn/b/buttonLite.js#style=-1&amp;uuid=&amp;pophcol=2&amp;lang=zh\"></script><script type=\"text/javascript\" charset=\"utf-8\" src=\"http://static.bshare.cn/b/bshareC0.js\"></script>");
+
+document.writeln("<br/>");
