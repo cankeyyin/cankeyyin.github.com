97168f9f68bfa9d48919b3bff5f10d539f5e696a
cankeyyin <yellow@yellowmatoMacBook-Air.local>
2013-12-23 14:29:48 +0800

    footer测试

--- footer.js
+++ footer.js
@@ -1,8 +1,5 @@
 
-<hr />
-
-<p>© 2012 天香嘉韵.</p>
-<script type="text/javascript">
+document.writeln("<hr />");
+document.writeln("<p>© 2012 天香嘉韵.</p>");
 var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
 document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F91c4162d2211f5bbead7f3b8f12fbd8d' type='text/javascript'%3E%3C/script%3E"));
-</script>
