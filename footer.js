badfb070cd26832322fd1418c5c6e095330c71c1
cankeyyin <yellow@yellowmatoMacBook-Air.local>
2013-12-23 15:26:27 +0800

    更新分享

--- footer.js
+++ footer.js
@@ -1,7 +1,5 @@
 <!--分享-->
-document.writeln("<div class=\"bshare-custom\"><a title=\"分享到\" href=\"http://www.bShare.cn/\" id=\"bshare-shareto\" class=\"bshare-more\">分享到</a><a title=\"分享到QQ空间\" class=\"bshare-qzone\">QQ空间</a><a title=\"分享到新浪微博\" class=\"bshare-sinaminiblog\">新浪微博</a><a title=\"分享到人人网\" class=\"bshare-renren\">人人网</a><a title=\"分享到腾讯微博\" class=\"bshare-qqmb\">腾讯微博</a><a title=\"分享到网易微博\" class=\"bshare-neteasemb\">网易微博</a><a title=\"更多平台\" class=\"bshare-more bshare-more-icon more-style-addthis\"></a><span class=\"BSHARE_COUNT bshare-share-count\">0</span></div>");
-document.writeln("<script type=\"text/javascript\" charset=\"utf-8\" src=\"http://static.bshare.cn/b/buttonLite.js#style=-1&amp;uuid=&amp;pophcol=2&amp;lang=zh\"></script>");
-document.writeln("<script type=\"text/javascript\" charset=\"utf-8\" src=\"http://static.bshare.cn/b/bshareC0.js\"></script>");
+document.writeln("<div class=\"bshare-custom\"><a title=\"分享到\" href=\"http://www.bShare.cn/\" id=\"bshare-shareto\" class=\"bshare-more\">分享到</a><a title=\"分享到QQ空间\" class=\"bshare-qzone\">QQ空间</a><a title=\"分享到新浪微博\" class=\"bshare-sinaminiblog\">新浪微博</a><a title=\"分享到人人网\" class=\"bshare-renren\">人人网</a><a title=\"分享到腾讯微博\" class=\"bshare-qqmb\">腾讯微博</a><a title=\"分享到网易微博\" class=\"bshare-neteasemb\">网易微博</a><a title=\"更多平台\" class=\"bshare-more bshare-more-icon more-style-addthis\"></a><span class=\"BSHARE_COUNT bshare-share-count\">0</span></div><script type=\"text/javascript\" charset=\"utf-8\" src=\"http://static.bshare.cn/b/buttonLite.js#style=-1&amp;uuid=&amp;pophcol=2&amp;lang=zh\"></script><script type=\"text/javascript\" charset=\"utf-8\" src=\"http://static.bshare.cn/b/bshareC0.js\"></script>");
 
 document.writeln("<hr />");
 document.writeln("<p>© 2012 天香嘉韵.</p>");
