d0f8ae0d6acb67feceb9b9301c1340931cbd09a6
cankeyyin <yellow@yellowmatoMacBook-Air.local>
2013-12-27 10:51:29 +0800

    新标签打开淘宝链接

--- header.js
+++ header.js
@@ -2,7 +2,7 @@
 document.writeln("<p><a href=\"index.html\">返回首页</a></p>");
 document.writeln("");
 
-
+/*
 document.write("<div align=\"center\">")
 document.write("<table width=\"100%\"");
 document.writeln("<tr>");
@@ -18,8 +18,9 @@ document.writeln("</tr>");
 document.write("</table>");
 document.write("</div>")
 document.writeln("");
+*/
 
-<!--分享页面-->
+/*分享页面*/
 document.writeln("<div class=\"bshare-custom\"><a title=\"分享到\" href=\"http://www.bShare.cn/\" id=\"bshare-shareto\" class=\"bshare-more\">分享到</a><a title=\"分享到新浪微博\" class=\"bshare-sinaminiblog\">新浪微博</a><a title=\"分享到微信\" class=\"bshare-weixin\">微信</a><a title=\"分享到QQ空间\" class=\"bshare-qzone\">QQ空间</a><a title=\"分享到人人网\" class=\"bshare-renren\">人人网</a><a title=\"分享到天涯\" class=\"bshare-tianya\">天涯</a><a title=\"更多平台\" class=\"bshare-more bshare-more-icon more-style-addthis\"></a><span class=\"BSHARE_COUNT bshare-share-count\">0</span></div><script type=\"text/javascript\" charset=\"utf-8\" src=\"http://static.bshare.cn/b/buttonLite.js#style=-1&amp;uuid=&amp;pophcol=2&amp;lang=zh\"></script><script type=\"text/javascript\" charset=\"utf-8\" src=\"http://static.bshare.cn/b/bshareC0.js\"></script>");
 
 document.writeln("<br/>");
